e441dd272354741ec0ea7654fcc032203ab4c913
Jeff Minnear <jeffminnear@gmail.com>
2016-08-17 10:02:25 -0400

    Add renderList function to display tasks

--- src/main.js
+++ src/main.js
@@ -6,12 +6,34 @@ import {
 } from 'react-native';
 
 module.exports = React.createClass({
+  getInitialState() {
+    return ({
+      tasks: ['Take out the trash', 'Get groceries', 'Practice piano']
+    })
+  },
+
+  renderList(tasks) {
+    // return individual rows based on the argued tasks
+    return(
+      tasks.map((task) => {
+        return (
+          <View key={task} style={styles.task}>
+            <Text>
+              {task}
+            </Text>
+          </View>
+        )
+      })
+    )
+  },
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>
+        <Text style={styles.header}>
           ToDo Master
         </Text>
+        {this.renderList(this.state.tasks)}
       </View>
     );
   }
@@ -20,6 +42,17 @@ module.exports = React.createClass({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    margin: 30,
+    marginTop: 40,
+    textAlign: 'center',
+    fontSize: 18
+  },
+  task: {
+    height: 60,
+    borderWidth: 1,
+    borderColor: 'black',
     justifyContent: 'center',
     alignItems: 'center'
   }
