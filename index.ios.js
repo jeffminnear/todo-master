53555cc735bec242560ca5474d51095ef15653ef
Jeff Minnear <jeffminnear@gmail.com>
2016-08-17 09:26:48 -0400

    Add basic Main component

--- index.ios.js
+++ index.ios.js
@@ -12,21 +12,12 @@ import {
   View
 } from 'react-native';
 
+import Main from './src/main';
+
 class ToDoMaster extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Main />
     );
   }
 }
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+module.exports = React.createClass({
+  render() {
+    return (
+      <View>
+        <Text>
+          ToDo Master
+        </Text>
+      </View>
+    );
+  }
+});
